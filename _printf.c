764e47112f038f806fab5d3226b3682884122144
Albaryys <a.tily.pro@gmail.com>
2023-03-30 23:50:44 -0700

    fixed some bug

--- _printf.c
+++ _printf.c
@@ -8,38 +8,40 @@
  */
 int _printf(const char *format, ...)
 {
-    int i, j, len = 0;
-    va_list args;
+	int i, j, len = 0;
+	va_list args;
 
-    print symb[] = {
-        {"c", print_c}, {"s", print_s}, {"%", print_p},
-        {"d", print_int}, {"i", print_int},
-        {NULL, NULL}};
+	print symb[] = {
+		{"c", print_c}, {"s", print_s}, {"%", print_p},
+		{"d", print_int}, {"i", print_int},
+		{NULL, NULL}};
 
-    va_start(args, format);
-    if (format == 0 || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            while (format[i] == ' ')
-                i++;
+	va_start(args, format);
+	if (format == 0 || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			while (format[i] == ' ')
+				i++;
 
-            for (j = 0; symb[j].symbol != NULL; j++)
-            {
-                if (format[i] == *symb[j].symbol)
-                {
-                    len += symb[j].function(args);
-                    break;
-                }
-            }
-        }
-        else
-            len += _putchar(format[i]);
-    }
+			for (j = 0; symb[j].symbol != NULL; j++)
+			{
+				if (format[i] == *symb[j].symbol)
+				{
+					len += symb[j].function(args);
+					break;
+				}
+			}
+		}
+		else
+		{
+			len += _putchar(format[i]);
+		}
+	}
 
-    va_end(args);
-    return (len);
+	va_end(args);
+	return (len);
 }
